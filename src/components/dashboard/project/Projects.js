9b05176578c97d50c309bc5f3d7c5fb166576aac
Antonio Silva <antonioesilva@gmail.com>
2020-07-07 18:14:14 +0200

    redirects for netlify

--- src/components/dashboard/project/Projects.js
+++ src/components/dashboard/project/Projects.js
@@ -1,23 +1,26 @@
 import React, { useContext } from "react";
-// import { useRouteMatch } from "react-router-dom";
 import CardProjects from "../cards/CardProjects";
-// import CardTranslators from "../cards/CardTranslators";
 import UserContext from "../../../contexts/UserContext";
 var uniqid = require('uniqid');
 
 const Project = () => {
-  // const matchTranslatorsRoute = useRouteMatch({
-  //   path: "/projects",
-  // });
   
   const { userProjects } = useContext(UserContext);
-  
+  console.log(userProjects)
   return (
+    <>
     <div className="body-project">
+  {userProjects.length !== 0 ?
+    <>
     {userProjects.map(project => 
         <CardProjects key={uniqid()} id={project._id} projectname={project.projectname} baselang={project.baselang} langs={project.langs} deadline={project.deadline}/>
       )}
+    </>
+    :
+    <div>Looks like you have no projects yet</div>
+    }
     </div>
+    </>
   );
 };
 
