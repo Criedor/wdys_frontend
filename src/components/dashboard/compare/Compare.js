687578cec117c7eb95a55a5874ed6978cfb76bcb
criedor <gawen@t-online.de>
2020-07-07 23:00:11 +0200

    added compare

--- src/components/dashboard/compare/Compare.js
+++ src/components/dashboard/compare/Compare.js
@@ -1,55 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom'
+import Axios from 'axios'
 import "../cards/Card.css";
 import "../tables/Tables.css";
 import "../Dashboard.css";
 import "./Compare.css";
+let uniqid = require('uniqid');
 
-function createData(pageName, status, assigned, due, view) {
-  return { pageName, status, assigned, due, view };
-}
-
-const rows = [
-  createData("Frozen yoghurt", "open", "2020/07/07", "7 Days", "X"),
-  createData("Ice cream sandwich", "review", "2020/07/07", "7 Days", "X"),
-  createData("Eclair", "open", "2020/07/07", "7 Days", "X"),
-  createData("Cupcake", "done", "2020/06/07", "done", "X"),
-  createData("Gingerbread", "done", "2020/06/07", "done", "X"),
-];
 
 const Compare = () => {
+  const { pageID } = useParams()
+  const [translationPage, setTranslationPage] = useState()
+  const [compare, setCompare] = useState()
+  const rows = []
+
+  useEffect(()=>{
+    Axios
+      .get(`https://wdys.herokuapp.com/translation/pages/${pageID}`)
+      .then((res) => { 
+        console.log(res)
+        setTranslationPage(res.data.translationpage)
+        setCompare(res.data.result)
+      })
+      .catch((err) => console.log(err))
+  },[pageID])
+
+
+
   return (
+    <>
+    {translationPage && compare &&
     <div className="body-compare">
-      <div>
-        <div className="title-gray title-green  white">
-          <div>Page Name</div>
-          <div>GER</div>
+      <div className="title-gray title-green  white">
+        <div>{translationPage.pagename}</div>
         </div>
         <div className="compare">
-          <div>
             <div className="title-gray">
               <div>Original</div>
             </div>
-            <div className="mt1"></div>
-            {rows.map((item) => (
-              <div className="compare-txt" key={item.pageName}>
-                {item.pageName}
-              </div>
-            ))}
-          </div>
-          <div>
             <div className="title-gray">
               <div>Translation</div>
             </div>
-            <div className="mt1"></div>
-            {rows.map((item) => (
-              <div className="compare-txt" key={item.pageName}>
-                {item.pageName}
+            {compare.map((item) => (
+              <>
+              <div className={`compare-txt ${item.depth>0?"child":""}`} key={uniqid()}>
+                {item.baseText}
               </div>
+              <div className={`compare-txt ${item.depth>0?"child":""}`} key={uniqid()}>
+                {item.translation}
+              </div>
+              </>
             ))}
           </div>
-        </div>
-      </div>
-    </div>
+      </div>}
+    </>
   );
 };
 
