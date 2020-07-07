6ea6af2cae53d6e74622e32d6744aa57d05152d8
Antonio Silva <antonioesilva@gmail.com>
2020-07-07 17:09:16 +0200

    COMMIT TO MERGE

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,13 +1,40 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { useContext} from "react";
+import { Route, Redirect } from "react-router-dom";
+import UserContext from "../contexts/UserContext";
 import Cookies from "js-cookie";
+import Axios from "axios"
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
+  const { setUserId, setUserName, setRole } = useContext(UserContext);
+
+  const login = () => {
+  
+    Axios.put("https://wdys.herokuapp.com/verify", {token: Cookies.get('token'), user_id: Cookies.get('user_id')})
+    .then((res) => {
+      setUserId(Cookies.get('user_id'));
+      setUserName(res.data.displayname);
+      setRole(Cookies.get('role'));
+      return res
+    })
+    .catch((err) => {
+
+        return <Route to={{pathname: "/"}}/>
+      })
+  };
+  
+
   if (Cookies.get("token")) {
+    if ( ()=>login().data.err !== "Invalid token")
+    login()
     return (
       <Route {...rest} render={(props) => <Component {...rest} {...props} />} />
     );
-  } else return "error or session expired";
-};
+  } 
+  else {
+    return (
+      <Redirect to='./' />
+    );
+  }
+}
 
 export default ProtectedRoute;
