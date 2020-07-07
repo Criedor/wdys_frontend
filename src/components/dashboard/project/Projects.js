9c35b89d389c3cf91d32de94bb8460224614be83
Antonio Silva <antonioesilva@gmail.com>
2020-07-07 20:09:52 +0200

    landing continued and empty page messages

--- src/components/dashboard/project/Projects.js
+++ src/components/dashboard/project/Projects.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import CardProjects from "../cards/CardProjects";
 import UserContext from "../../../contexts/UserContext";
+import { ReactComponent as AddProject } from './add-project.svg'
 var uniqid = require('uniqid');
 
 const Project = () => {
@@ -9,17 +10,21 @@ const Project = () => {
   console.log(userProjects)
   return (
     <>
-    <div className="body-project">
-  {userProjects.length !== 0 ?
-    <>
-    {userProjects.map(project => 
-        <CardProjects key={uniqid()} id={project._id} projectname={project.projectname} baselang={project.baselang} langs={project.langs} deadline={project.deadline}/>
-      )}
-    </>
-    :
-    <div>Looks like you have no projects yet</div>
+    {userProjects.length !== 0 ?
+      <div className="body-project">
+        {userProjects.map(project => 
+            <CardProjects key={uniqid()} id={project._id} projectname={project.projectname} baselang={project.baselang} langs={project.langs} deadline={project.deadline}/>
+          )}
+      </div>
+      :
+      <div className='no-table'>
+        <div className='center'>
+          <AddProject style={{maxWidth: '400px', maxHeight: '300px'}}/>
+          <p className='center'>Looks like you have no projects yet, get started by adding one.</p>
+        </div>
+      </div>
     }
-    </div>
+    
     </>
   );
 };
